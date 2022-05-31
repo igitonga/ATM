4994fe8d5c93dd23d158fcbaca6da39afc5fa4c5
Juma-h <50380658+igitonga@users.noreply.github.com>
2022-05-31 20:30:41 +0300

    withdraw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,8 @@ public class Main {
     static int pin;
     static int menu;
     static int amtWithdraw;
+    static int newBalance;
+    static int confirm;
 
     static Scanner scanner = new Scanner(System.in);
 
@@ -51,12 +53,12 @@ public class Main {
         System.out.println("2. Withdraw");
         menu = scanner.nextInt();
         switch (menu){
-            case '1':
-                withdraw();
+            case 1:
+                balance();
                 break;
 
-            case '2':
-                balance();
+            case 2:
+                withdraw();
                 break;
 
             default:
@@ -69,6 +71,25 @@ public class Main {
     static void withdraw(){
         System.out.println("How much would you like to withdraw?");
         amtWithdraw = scanner.nextInt();
+        if(amtWithdraw >= balance){
+            System.out.println("Insufficient funds");
+            menu();
+        }
+        else{
+            System.out.println("Confirm that you want to withdraw "+amtWithdraw);
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+            confirm = scanner.nextInt();
+            switch (confirm){
+                case 1:
+                    success();
+                    break;
+
+                case 2:
+                    System.out.println("Withdrawal cancelled");
+                    menu();
+            }
+        }
     }
 
     static  void balance(){
@@ -76,6 +97,17 @@ public class Main {
         System.out.println("Name: "+name);
         System.out.println("Account number: 01937772410");
         System.out.println("Amount available: "+balance);
+        System.out.println("===========Thank you for banking with us==========");
+    }
+
+    static void success(){
+        newBalance = balance - amtWithdraw;
+
+        System.out.println("=============Gee Bank=============");
+        System.out.println("Name: "+name);
+        System.out.println("Account number: 01937772410");
+        System.out.println("Amount withdrawn: "+amtWithdraw);
+        System.out.println("Balance: "+newBalance);
         System.out.println("===========Thank you for banking with us==========");
     }
 
